34ecd89f7a024ed10a01a9d0259c2276ee28c37f
ingaku <ingaku@slack-corp.com>
2021-06-09 12:43:36 -0400

    minor change

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,17 @@
-import * as React from "react"
+import { StaticImage } from 'gatsby-plugin-image'
+import * as React from 'react'
+
 import Layout from '../components/layout'
-import { StaticImage } from "gatsby-plugin-image"
 
 // markup
-const IndexPage = () => {
-  return (
-    <Layout pageTitle="Home Page">
-      <p>I'm making this by following the Gatsby Tutorial.</p>
-      <StaticImage
-        alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
-        src="https://pbs.twimg.com/media/E1oMV3QVgAIr1NT?format=jpg&name=large"
-      />
-    </Layout>
-  )
-}
+const IndexPage =
+  () => {
+    return (
+      <Layout pageTitle='Home Page'>
+        <p>I'm making this by following the Gatsby Tutorial.</p>
+        <StaticImage src="https://placekitten.com/800/600" alt="A kitten" />
+      </Layout>
+    )
+  }
 
 export default IndexPage
