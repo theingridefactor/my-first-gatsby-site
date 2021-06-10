48de633f3c8cf5cfc83bcbf81e167786b5ebff74
ingaku <ingaku@slack-corp.com>
2021-06-10 09:31:48 -0400

    used airtable data in project

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+
 module.exports = {
   siteMetadata: {
     title: "My First Gatsby Site",
@@ -7,5 +9,17 @@ module.exports = {
     `gatsby-plugin-image`,
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`, // Needed for dynamic images
+    {
+      resolve: "gatsby-source-airtable",
+      options: {
+        apiKey: process.env.AIRTABLE_API_KEY,
+        tables: [
+          {
+            tableName: "Notes",
+            baseId: "apprQtkDtymwX7x78",
+          },
+        ],
+      },
+    },
   ],
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,37 @@
 import { StaticImage } from 'gatsby-plugin-image'
 import * as React from 'react'
 
+import { graphql } from 'gatsby';
+
 import Layout from '../components/layout'
+
+export const query = graphql`query MyQuery {
+  allAirtable {
+    nodes {
+      data {
+        Link
+        Notes
+        Title
+      }
+    }
+  }
+}`
 
 // markup
 const IndexPage =
-  () => {
+  ({ data }) => {
     return (
+      <div>
       <Layout pageTitle='Home Page'>
         <p>I'm making this by following the Gatsby Tutorial.</p>
         <StaticImage src="https://placekitten.com/800/600" alt="A kitten" />
       </Layout>
+      {data.allAirtable.nodes.map((node) => 
+        <div>
+          <p>{node.data.Title}</p>
+          <p>{node.data.Notes} <a href={node.data.Link}>>>>> Go Now</a></p>
+        </div>)}
+        </div>
     )
   }
 
